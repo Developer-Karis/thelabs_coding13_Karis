09a6769b9904fe018905ac50ccf961d71905a897
Karis Papadopoulos <60817321+SEVERECLICK-dev@users.noreply.github.com>
2021-01-04 11:35:14 +0100

    structure de base

---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ServiceController;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\BlogPostController;
+use App\Http\Controllers\ContactController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('labs.home');
+});
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', function() {
+    return view('home');
+})->name('home')->middleware('auth');
+
+// Service
+Route::get('/services', [ServiceController::class, 'index']);
+
+// Blog
+Route::get('/blog', [BlogController::class, 'index']);
+
+// Blog-Post
+Route::get('/blog-post', [BlogPostController::class, 'index']);
+
+// Contact
+Route::get('/contact', [ContactController::class, 'index']);
