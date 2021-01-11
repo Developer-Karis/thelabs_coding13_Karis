55db87d3ad60198c023b4e0f5c4a497080bb94e2
Karis Papadopoulos <60817321+SEVERECLICK-dev@users.noreply.github.com>
2021-01-11 21:49:52 +0100

    work in progress blog

--- resources/views/labs/blog.blade.php
+++ resources/views/labs/blog.blade.php
@@ -29,13 +29,13 @@
 </head>
 
 <body>
-	{{-- <!-- Page Preloder -->
+	<!-- Page Preloder -->
 	<div id="preloder">
 		<div class="loader">
 			<img src="img/logo.png" alt="">
 			<h2>Loading.....</h2>
 		</div>
-	</div> --}}
+	</div>
 
 
 	<!-- Header section -->
